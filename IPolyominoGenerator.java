ef3cb25d8f631d00b56eb3db4c3e564357d2a22e
Vaibhav Malhotra <vbvrocks84@gmail.com>
2016-09-19 01:25:28 -0500

    Add files via upload

---
+++ IPolyominoGenerator.java
@@ -0,0 +1,14 @@
+package tetris.api;
+
+/**
+ * Abstraction of a generator for game pieces in a Tetris-like
+ * video game.
+ */
+public interface IPolyominoGenerator {
+	/**
+	 * Returns a new IPolyomino instance according to generator's
+	 * strategy.
+	 * @return a new IPolyomino
+	 */
+	IPolyomino getNext();
+}
